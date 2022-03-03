8eca14e61865d912cbef5ff39384a86780b03b15
18016447 <Smith201!>
2022-03-03 08:12:54 +0100

    Q5 done

--- interaction.js
+++ interaction.js
@@ -2,11 +2,47 @@
 // La création d'un Dnd requière un canvas et un interacteur.
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+  // Définir ici les attributs de la 'classe'
+  this.initX = 0;
+  this.initY = 0;
+  this.finalX = 0;
+  this.finalY = 0;
+  this.click = false;
+  // Developper les 3 fonctions gérant les événements
 
-	// Developper les 3 fonctions gérant les événements
+  this.maFctGérantLaPression = function(evt){
+    var position = getMousePosition(canvas, evt);
+    this.click = true;
+    this.initX = position.x;
+    this.initY = position.y;
+    console.log(position);
+  
+  }.bind(this);
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+  this.maFctGérantLeDéplacement = function(evt){
+    if (this.click) {
+      var position = getMousePosition(canvas, evt);
+    this.click = true;
+    this.finalX = position.x;
+    this.finalY = position.y;
+    console.log(position);
+    }
+  }.bind(this);
+
+  this.maFctGérantLeRelâchement = function(evt){
+    var position = getMousePosition(canvas, evt);
+    this.click = false;
+    this.finalX = position.x;
+    this.finalY = position.y;
+    console.log(position);
+  }.bind(this);
+
+  // Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.maFctGérantLaPression, false);
+  canvas.addEventListener('mousemove', this.maFctGérantLeDéplacement, false);
+  canvas.addEventListener('mouseup', this.maFctGérantLeRelâchement, false);
+
+
 };
 
 
@@ -18,6 +54,9 @@ function getMousePosition(canvas, evt) {
     y: evt.clientY - rect.top
   };
 };
+
+  
+
 
 
 
