3677c07f19ce0ebed7746f5343a9881c93a57a28
18016447 <Smith201!>
2022-03-03 08:56:20 +0100

    Q6 done

--- model.js
+++ model.js
@@ -1,3 +1,32 @@
 
 // Implémenter ici les 4 classes du modèle.
+
+function Drawing(){
+    this.list = new Map();
+
+};
+
+function Forme(initX,initY,couleur,epaisseur){
+    this.couleur = couleur;
+    this.epaisseur = epaisseur;
+    this.initX = initX;
+    this.initY=initY;
+};
+
+
+function Line(initX,initY,couleur,epaisseur,endX,endY){
+    Forme.call(this,initX,initY,couleur,epaisseur);
+    this.endX = endX;
+    this.endY = endY;
+};
+Line.prototype = new Forme();
+
+function Rectangle(initX,initY,couleur,epaisseur,largeur,hauteur){
+    Forme.call(this,initX,initY,couleur,epaisseur);
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+
+};
+Rectangle.prototype = new Forme();
+
 // N'oubliez pas l'héritage !
