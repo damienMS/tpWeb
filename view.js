b42de884a543cecab225ce10b9fc6da4c14cbc1c
FRXZ3N <benjamin.ramone@outlook.fr>
2022-03-03 11:12:01 +0100

    Resolving merge conflicts

--- view.js
+++ view.js
@@ -1,5 +1,6 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+<<<<<<< Updated upstream
 Drawing.prototype.paint = function(ctx,canvas) {
     console.log(this.getForms());
     ctx.fillStyle = '#F0F0F0'; // set canvas' background color
@@ -47,3 +48,34 @@ Rectangle.prototype.paint = function(ctx) {
   
   
   
+=======
+Drawing.prototype.paint = function(ctx) {
+    //console.log(this.getForms());
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.getForms().forEach(function (eltDuTableau) {
+      // now fill the canvas
+      eltDuTableau.paint(ctx);
+    });
+    
+Forme.prototype.paint = function(ctx) {
+    ctx.stroke();
+};
+
+Rectangle.prototype.paint = function(ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.rect(this.getInitX(), this.getInitY(), this.getFinalX(),   this.getFinalY());
+    ctx.stroke();
+  };
+  
+  Line.prototype.paint = function(ctx) {
+    //TODO Manager color
+    ctx.beginPath();
+    ctx.moveTo(this.getInitX(), this.getInitY());
+    ctx.lineTo(this.getFinalX(), this.getFinalY());
+    ctx.stroke();
+  };
+
+};
+>>>>>>> Stashed changes
